510e82be1e8f7966ef5eadcd7ee97dd167b3c637
Joseph Darby <jd21726@essex.ac.uk>
2024-01-21 17:50:05 +0000

    Completed MapParser and implemented it into the program.

--- Capstone Project/Fundamentals/Array2D.cs
+++ Capstone Project/Fundamentals/Array2D.cs
@@ -1,9 +1,11 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+
 namespace Capstone_Project.Fundamentals
 {
-    public class Array2D<T>
+    public class Array2D<T> : ICollection<T>, IEnumerable<T>
     {
-        private readonly T[] arr;
+        private T[] arr { get; set; }
         public int Width { get; init; }
         public int Height { get; init; }
 
@@ -21,9 +23,60 @@ namespace Capstone_Project.Fundamentals
             arr = new T[Width * Height];
         }
 
-        public T this[int x, int y] => arr[x + (y * Width)];
-        public T this[int i] => arr[i];
+        public T this[int x, int y]
+        {
+            get => arr[x + (y* Width)];
+            set => arr[x + (y * Width)] = value;
+        }
+        public T this[int i]
+        {
+            get => arr[i];
+            set => arr[i] = value;
+        }
         public (int x, int y) FindPosition(int index) => new(index % Width, index / Width);
+
+        public void Add(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            System.Array.Clear(arr);
+        }
+
+        public bool Contains(T item)
+        {
+            foreach (T x in arr)
+                if (item.Equals(x))
+                    return true;
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            arr.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (IEnumerator<T>)arr.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return arr.GetEnumerator();
+        }
+
         public int Length => arr.Length;
+
+        public int Count => arr.Length;
+
+        public bool IsReadOnly => false;
     }
 }
